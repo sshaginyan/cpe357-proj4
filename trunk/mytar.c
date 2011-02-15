7d809991330fd4bc4382d9de14bda877c88516fd
luis.calpoly@gmail.com <luis.calpoly@gmail.com@8e646b74-bef1-0aed-0107-ebed2789b9b1>
2011-02-15 07:07:47 +0000

    Created project repository

---
+++ trunk/mytar.c
@@ -0,0 +1,34 @@
+/**
+ * @file
+ * <pre> CPE 357 Winter 2011
+ * -------------------
+ *
+ * Program "describe the program here (without quotes)"
+ *
+ * Last Modified: Wed Jan 29 13:39:06 PST 2011</pre>
+ * @author Luis Castillo
+ * Copyright (C) 2011 Luis Castillo. All rights reserved.
+ */
+
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+/** The main thing.
+ * @param argc the number of tokens on the input line.
+ * @param argv an array of tokens.
+ * @return 0 on success, 1-255 on failure
+ */
+int
+main(int argc, char *argv[])
+{
+  
+  return EXIT_SUCCESS;
+}
+
+/* vim: set et ai sts=2 sw=2: */
