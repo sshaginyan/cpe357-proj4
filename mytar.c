ae86b91ffce66116b6446ad34f195bf3ba5a6587
luis.calpoly <luis.calpoly@gmail.com>
2011-02-16 23:05:12 +0000

    included comments for documentation

--- mytar.c
+++ mytar.c
@@ -1,10 +1,34 @@
-#include <stdio.h>
+/**
+ * @file
+ * <pre> CPE 357 Winter 2011
+ * -------------------
+ *
+ * Program "describe the program here (without quotes)"
+ *
+ * Last Modified: Wed Feb 16 13:39:06 PST 2011</pre>
+ * @author Luis Castillo
+ * Copyright (C) 2011 Luis Castillo. All rights reserved.
+ */
+
+#include <sys/types.h>
+#include <unistd.h>
+
 #include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
 #include <getopt.h>
 
 /* Flags set by --help and --version */
 static int help_flag, version_flag;
 
+/** The main thing.
+ * @param argc the number of tokens on the input line.
+ * @param argv an array of tokens.
+ * @return 0 on success, 1-255 on failure
+ */
 int main (int argc, char *argv[])
 {
   int c;
@@ -103,3 +127,4 @@ int main (int argc, char *argv[])
   return EXIT_SUCCESS;
 }
 
+/* vim: set et ai sts=2 sw=2: */
