340f70df659710f51a312d6feb3b63f569c12147
luis.calpoly <luis.calpoly@gmail.com>
2011-02-23 22:23:29 +0000

    Submitting new tarFunctions c and h files and adding tar.h file

--- tarFunctions.c
+++ tarFunctions.c
@@ -10,6 +10,50 @@
  * @author Luis Castillo, Ricardo Tijero
  */
 
+/***************************************************************************************/
+/* DEFINITIONS:
+ *
+ *      BUF_SIZE     100
+ *      BLOCK_SIZE   512
+ *      NAME_SIZE    100
+ *      MODE_SIZE      8
+ *      UID_SIZE       8
+ *      GID_SIZE       8
+ *      FILE_SIZE     12
+ *      TIME_SIZE     12
+ *      CHKSM_SIZE     8
+ *      LNKNAME_SIZE 100
+ *      USTAR_SIZE     6
+ *      VER_SIZE       2
+ *      UNAME_SIZE    32
+ *      GNAME_SIZE    32
+ *      DEVNUM_SIZE    8
+ *      PREFIX_SIZE  155
+ *
+ *
+ * STRUCTURES USED:
+ *
+ *   struct header{                    <SIZE>
+ *       char fileName[NAME_SIZE];      (100)
+ *       char mode[MODE_SIZE];          (  8)
+ *       char userID[UID_SIZE];         (  8)
+ *       char groupID[GID_SIZE];        (  8)
+ *       char fileSize[FILE_SIZE];      ( 12)
+ *       char time[TIME_SIZE];          ( 12)
+ *       char checksum[CHCKSM_SIZE];    (  8)
+ *       char fileType;                 (  1)
+ *       char linkName[LNKNAME_SIZE];   (100)
+ *       char ustar[USTAR_SIZE];        (  6)
+ *       char version[VER_SIZE];        (  2)
+ *       char userName[UNAME_SIZE];     ( 32)
+ *       char groupName[GNAME_SIZE];    ( 32)
+ *       char devMajNum[DEVNUM_SIZE];   (  8)
+ *       char devMinNum[DEVNUM_SIZE];   (  8)
+ *       char prefix[PREFIX_SIZE];      (155)
+ *   };  
+ *                                                                                     */
+/***************************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -18,6 +62,114 @@
 #include <fcntl.h>
 #include "tarFunctions.h"
 
+/***************************************************************************************/
+/* Brif: Creates new header structure and returns a pointer to it.                     */
+/* Details: Given an array of 512 characters (one Block), it creates a header structure*/
+/*    and initializes every field in the structure. Returns a pointer to structure.    */
+/***************************************************************************************/
+header *newHeader(char *buf)
+{
+    int i;
+    header *tarHeader;
+    header *pos;
+    
+    tarHeader = malloc (sizeof (struct header));
+    if (tarHeader == NULL)
+    {
+        perror("malooc");
+        return NULL;
+    }
+
+    pos = tarHeader;
+
+    for (i = 0; i < BLOCK_SIZE; i++)
+    {
+        *pos = buf[i];
+        pos++;
+    }
+    return tarHeader;
+}
+
+/***************************************************************************************/
+/* Description: gets a header from a tar file and places pointer to next the next      */
+/*     header in the file.                                                             */
+/***************************************************************************************/
+header *nextHeader (int fd)
+{
+    int n, fullFile;
+    int count = 0;
+    char buf[BLOCK_SIZE];
+    char aHeader[BLOCK_SIZE];
+    
+    if ((n = read(fd, aHeader, BLOCK_SIZE)) <= 0)
+        return NULL;
+
+    while (!fullFile && (n = read(fd, buf, BLOCK_SIZE)) > 0)
+    {
+        if (isNullBlock(buf))
+            count++;
+        else
+            count = 0;
+
+        if (count == 2)
+            fullFile = 1;
+    }
+
+    return newHeader (aHeader);
+}
+
+/***************************************************************************************/
+/* Description: given an array, determines if it is full of NULLs.                     */
+/***************************************************************************************/
+int isNullBlock (char *block)
+{
+    int i;
+    int count = 0;
+
+    for (i = 0; i < BLOCK_SIZE; i++)
+    {
+        if (block[i] == '\0')
+            count++;
+    }
+    
+    if (count == BLOCK_SIZE)
+        return 1;
+    else
+        return 0;
+}
+
+/***************************************************************************************/
+/* Description: changes a number represented in ASCII characters to intigers.          */
+/***************************************************************************************/
+int charToInt (char *arr, int leng)
+{
+    int num = 0;
+    int i;
+
+    for (i = 0; i < leng; i++)
+    {
+        num = (num * 10) + (arr[i] - '0');
+    }
+
+    return num;
+}
+/*>>>>>>>>>>>>>>>>>>>>>>  NEEDS REVISION <<<<<<<<<<<<<<<<<<<<<<<<<<*/
+/***************************************************************************************/
+/* Description: Displays the names of the files in an array. (myTar option -t)         */
+/***************************************************************************************/
+void printFiles (header *files[], int leng)
+{
+    int i;
+
+    for (i = 0; i < leng; i++)
+        printf("%s\n", files[i]->fileName); /* fileName NULL terminated? */
+
+    return;
+}
+
+/***************************************************************************************/
+/* Description: prints the help file to the screen.                                    */
+/***************************************************************************************/
 void printHelp ()
 {
   int fd;
@@ -42,6 +194,9 @@ void printHelp ()
   return;
 }
 
+/***************************************************************************************/
+/* Description: prints the version file to the screen.                                 */
+/***************************************************************************************/
 void printVersion ()
 {
   int fd;
--- tarFunctions.h
+++ tarFunctions.h
@@ -10,8 +10,51 @@
 #define TARFUNCTIONS_H
 
 #define BUF_SIZE 100
+#define BLOCK_SIZE 512
 
-void printHelp(void);
-void printVersion(void);
+/* Header components' sizes */
+#define NAME_SIZE 100
+#define MODE_SIZE 8
+#define UID_SIZE 8
+#define GID_SIZE 8
+#define FILE_SIZE 12
+#define TIME_SIZE 12
+#define CHKSM_SIZE 8
+#define LNKNAME_SIZE 100
+#define USTAR_SIZE 6
+#define VER_SIZE 2
+#define UNAME_SIZE 32
+#define GNAME_SIZE 32
+#define DEVNUM_SIZE 8
+#define PREFIX_SIZE 155;
+
+typedef struct header header;
+
+struct header{
+    char fileName[NAME_SIZE];
+    char mode[MODE_SIZE];
+    char userID[UID_SIZE];
+    char groupID[GID_SIZE];
+    char fileSize[FILE_SIZE];
+    char time[TIME_SIZE];
+    char checksum[CHCKSM_SIZE];
+    char fileType;
+    char linkName[LNKNAME_SIZE];
+    char ustar[USTAR_SIZE];
+    char version[VER_SIZE];
+    char userName[UNAME_SIZE];
+    char groupName[GNAME_SIZE];
+    char devMajNum[DEVNUM_SIZE];
+    char devMinNum[DEVNUM_SIZE];
+    char prefix[PREFIX_SIZE];
+};
+
+header *newHeader (char *buf);
+header *nextHeader (int fd);
+int isNullBlock (char *block);
+int charToInt (char *arr, int leng);
+void printFiles (header *files[], int leng);
+void printHelp (void);
+void printVersion (void);
 
 #endif
